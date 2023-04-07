d944439b93be7d873d82474e7e2a5f6b93b8c1f5
ryohidaka <39184410+ryohidaka@users.noreply.github.com>
2023-04-07 19:58:15 +0900

    feat: Add export config

--- next.config.js
+++ next.config.js
@@ -3,6 +3,8 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-}
+  // @see https://beta.nextjs.org/docs/configuring/static-export#configuration
+  output: "export",
+};
 
 module.exports = nextConfig

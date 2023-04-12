406aa645f9fc800bd353877974bcf83308ba26f4
ryohidaka <39184410+ryohidaka@users.noreply.github.com>
2023-04-12 09:27:46 +0900

    feat: Add sample component for rendering a list of posts
    This commit adds a new component that fetches a list of posts from a dummy JSON API and renders them. The component uses server-side rendering with server components and can be statically exported. The component includes fields for title, body, username, tags, and reactions for each post.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,41 @@
-export default function Home() {
-  return <main></main>;
+import { PostResponse } from "@/types";
+
+/**
+ * Sample component for rendering a list of posts
+ *
+ * @see https://beta.nextjs.org/docs/configuring/static-export#server-components
+ * @returns
+ */
+export default async function Page() {
+  // This fetch will run on the server during `next build`
+  const res = await fetch("https://dummyjson.com/posts");
+  const data: PostResponse = await res.json();
+  const posts = data.posts;
+
+  return (
+    <main>
+      {posts.map((post) => (
+        <div key={post.id} id={post.id.toString()}>
+          {/* Title */}
+          <h3 id="title">{post.title}</h3>
+
+          {/* Body */}
+          <p id="body">{post.body}</p>
+
+          {/* Username */}
+          <p id="username">User {post.userId}</p>
+
+          {/* Tags */}
+          <ul id="tags">
+            {post.tags.map((tag) => (
+              <li key={tag}>{tag}</li>
+            ))}
+          </ul>
+
+          {/* Reactions */}
+          <p id="reactions">{post.reactions}</p>
+        </div>
+      ))}
+    </main>
+  );
 }
